d69de3ac4614c2cfeec160512d5175450db00fbc
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-03-29 21:12:58 +0200

    add get_answers_statistics.py script

---
+++ get_answers_statistics.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+df_correct_answer = pd.read_csv('questions/solutions_complete.csv')
+df_user_answers = pd.read_csv('questions/answers_complete.csv')
+
+answers_statistics_dataframe = pd.DataFrame(columns=['ID', 'MEDIA_NAME', 'CORRECT_ANSWER'])
+answers_statistics_dataframe['ID'] = df_user_answers['ID']
+answers_statistics_dataframe['MEDIA_NAME'] = df_correct_answer['MEDIA_NAME']
+answers_statistics_dataframe['CORRECT_ANSWER'] = df_correct_answer['CORRECT_ANSWER']
+
+list = os.listdir('eye-tracker')
+number_files = int(len(list) / 2) + 1
+
+for i in range(1, number_files):
+
+    if not(i==25 or i==26 or i==30):
+
+        user_id = 'USER_' + str(i)
+        user_answers = 'users/answers/answer_user_' + str(i) + '.csv'
+
+        df_user = pd.read_csv (user_answers)
+        answers = dict.fromkeys(df_correct_answer['MEDIA_NAME'], 0)
+
+        for j in df_user.index:
+            answers[df_user['MEDIA_NAME'][j]] = df_user['USER_ANSWER'][j]
+
+        answers_statistics_dataframe[user_id] = answers.values()
+
+
+answers_statistics_dataframe.to_csv('results/answers_for_question.csv', index=False)
