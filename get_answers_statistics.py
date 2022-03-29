78dc6a4bef1e1c245b6498b1b3b0ce068152e245
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-03-29 21:47:24 +0200

    get_answers_statistics.py: compute ratios of right and wrong answers

--- get_answers_statistics.py
+++ get_answers_statistics.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 
+pd.options.mode.chained_assignment = None
+
 df_correct_answer = pd.read_csv('questions/solutions_complete.csv')
 df_user_answers = pd.read_csv('questions/answers_complete.csv')
 
@@ -29,3 +31,26 @@ for i in range(1, number_files):
 
 
 answers_statistics_dataframe.to_csv('results/answers_for_question.csv', index=False)
+
+statistics =  pd.DataFrame(columns=['ID','MEDIA_NAME','TOTAL','RIGHT','WRONG'])
+statistics['ID'] = answers_statistics_dataframe['ID']
+statistics['MEDIA_NAME'] = answers_statistics_dataframe['MEDIA_NAME']
+
+for i in answers_statistics_dataframe.index:
+    statistics['TOTAL'][i] = 0
+    statistics['RIGHT'][i] = 0
+    statistics['WRONG'][i] = 0
+
+for i in answers_statistics_dataframe.index:
+    for j in range(1, number_files):
+        if not (j == 25 or j == 26 or j == 30):
+            if answers_statistics_dataframe['CORRECT_ANSWER'][i] == answers_statistics_dataframe['USER_'+str(j)][i]:
+                statistics['RIGHT'][i] += 1
+            else:
+                statistics['WRONG'][i] += 1
+            statistics['TOTAL'][i] += 1
+
+for i in statistics.index:
+    n_right = (statistics['RIGHT'][i] / statistics['TOTAL'][i]) * 100
+    n_wrong = (statistics['WRONG'][i] / statistics['TOTAL'][i]) * 100
+    print(statistics['MEDIA_NAME'][i] + ' ' + str(n_right) + ' ' + str(n_wrong))
