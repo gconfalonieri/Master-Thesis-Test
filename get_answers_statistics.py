22cff6fbefaaab245f8ca40b4dc1ba4f78703e15
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-04-04 23:37:53 +0200

    add barplot in get_answers_statistics.py

--- get_answers_statistics.py
+++ get_answers_statistics.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
@@ -11,6 +13,9 @@ df_answers_for_question = pd.read_csv('results/answers_for_question.csv')
 df_questions_statistics = pd.DataFrame(columns=['ID','MEDIA_NAME','TOTAL','RIGHT','WRONG'])
 df_questions_statistics['ID'] = df_answers_for_question['ID']
 df_questions_statistics['MEDIA_NAME'] = df_answers_for_question['MEDIA_NAME']
+
+list_rights = []
+list_wrongs = []
 
 for i in df_answers_for_question.index:
     df_questions_statistics['TOTAL'][i] = 0
@@ -29,6 +34,21 @@ for i in df_answers_for_question.index:
 for i in df_questions_statistics.index:
     n_right = (df_questions_statistics['RIGHT'][i] / df_questions_statistics['TOTAL'][i]) * 100
     n_wrong = (df_questions_statistics['WRONG'][i] / df_questions_statistics['TOTAL'][i]) * 100
+    list_rights.append(n_right)
+    list_wrongs.append(n_wrong)
     print(df_questions_statistics['MEDIA_NAME'][i] + ' ' + str(n_right) + ' ' + str(n_wrong))
+
+print(list_rights)
+print(list_wrongs)
+
+barWidth = 0.25
+br1 = np.arange(len(list_rights))
+br2 = [x + barWidth for x in br1]
+
+plt.bar(br1, list_rights, color ='g', width = barWidth, edgecolor ='grey', label ='RIGHT')
+plt.bar(br2, list_wrongs, color ='r', width = barWidth, edgecolor ='grey', label ='WRONG')
+
+plt.legend()
+plt.savefig('results/img/questions_statistics.png')
 
 df_questions_statistics.to_csv('results/questions_statistics.csv', index=False)
