00757a4c369b101fb8d7d589c460adca733fdc55
Gianantonio Confalonieri <gianantonioconfalonieri@gmail.com>
2022-04-11 19:10:13 +0200

    get statistics for labelling

--- get_answer_labels.py
+++ get_answer_labels.py
@@ -11,5 +11,10 @@ median_dict = statistics.utilities.get_median_dict(answer_dict)
 df_answers_labelled_mean = statistics.set_labels.get_df_answers_labelled(df_complete, min_dict, mean_dict)
 df_answers_labelled_median = statistics.set_labels.get_df_answers_labelled(df_complete, min_dict, mean_dict)
 
+df_labelled_mean_statistics = statistics.set_labels.get_df_label_statistics(df_answers_labelled_mean)
+df_labelled_median_statistics = statistics.set_labels.get_df_label_statistics(df_answers_labelled_median)
+
 df_answers_labelled_mean.to_csv('datasets/results/labels_mean_answers_all_info.csv', index=False)
 df_answers_labelled_median.to_csv('datasets/results/labels_median_answers_all_info.csv', index=False)
+df_labelled_mean_statistics.to_csv('datasets/results/labels_mean_statistics_answers_all_info.csv', index=False)
+df_labelled_median_statistics.to_csv('datasets/results/labels_median_statistics_answers_all_info.csv', index=False)
--- statistics/set_labels.py
+++ statistics/set_labels.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 import statistics.utilities
 
 pd.options.mode.chained_assignment = None
@@ -18,7 +17,31 @@ def get_df_answers_labelled(df_complete, min_times_dict, threshold_dict):
             df_complete_labelled['LABEL'][i] = 'COGNITIVE_EFFORT'
         elif is_right_answer and answer_time > current_threshold:
             df_complete_labelled['LABEL'][i] = 'COGNITIVE_EFFORT'
-        elif is_right_answer and current_min_time < answer_time < current_threshold:
+        elif is_right_answer and current_min_time <= answer_time <= current_threshold:
             df_complete_labelled['LABEL'][i] = 'NOT_COGNITIVE_EFFORT'
 
     return df_complete_labelled
+
+
+def get_df_label_statistics(df_labelled):
+
+    df_label_statistics = pd.DataFrame(columns=['MEDIA_NAME', 'N_COGNITIVE_EFFORT', 'N_NOT_COGNITIVE_EFFORT'])
+    df_label_statistics['MEDIA_NAME'] = (pd.read_csv('datasets/questions/solutions_complete.csv'))['MEDIA_NAME']
+
+    effort_dict = dict()
+    not_effort_dict = dict()
+
+    for x in df_label_statistics['MEDIA_NAME']:
+        effort_dict[x] = 0
+        not_effort_dict[x] = 0
+
+    for i in df_labelled.index:
+        if df_labelled['LABEL'][i] == "COGNITIVE_EFFORT":
+            effort_dict[df_labelled['MEDIA_NAME'][i]] += 1
+        elif df_labelled['LABEL'][i] == "NOT_COGNITIVE_EFFORT":
+            not_effort_dict[df_labelled['MEDIA_NAME'][i]] += 1
+
+    df_label_statistics['N_COGNITIVE_EFFORT'] = effort_dict.values()
+    df_label_statistics['N_NOT_COGNITIVE_EFFORT'] = not_effort_dict.values()
+
+    return df_label_statistics
