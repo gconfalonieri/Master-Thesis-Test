490039bd58ea5b2df3d86df8d267decef207b6e3
Gianantonio Confalonieri <gianantonioconfalonieri@gmail.com>
2022-04-26 16:35:56 +0200

    refactor: compute standard deviation for questions average times

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ df_correct_answer = pd.read_csv(config['path']['solutions_complete_dataset'])
 
 # 1) Compute Answer DataFrame for each User
 
-statistics.questions_statistics.save_all_user_answers_df(n_users)
+# statistics.questions_statistics.save_all_user_answers_df(n_users)
 
 # 2) Compute DataFrame with all the answers related to a single question
 
-df_answers_for_questions = statistics.questions_statistics.get_answer_for_questions_df(df_correct_answer, n_users)
-df_answers_for_questions.to_csv('datasets/results/answers_for_questions.csv', index=False)
+# df_answers_for_questions = statistics.questions_statistics.get_answer_for_questions_df(df_correct_answer, n_users)
+# df_answers_for_questions.to_csv('datasets/results/answers_for_questions.csv', index=False)
 
 # 3) Compute DataFrame with some statistics related to a single question
 
-df_questions_statistics = statistics.questions_statistics.get_questions_statistics_df(df_answers_for_questions, n_users)
-df_questions_statistics.to_csv('datasets/results/questions_statistics.csv', index=False)
-statistics.plots.get_questions_statistics_bar_plot(df_questions_statistics)
+# df_questions_statistics = statistics.questions_statistics.get_questions_statistics_df(df_answers_for_questions, n_users)
+# df_questions_statistics.to_csv('datasets/results/questions_statistics.csv', index=False)
+# statistics.plots.get_questions_statistics_bar_plot(df_questions_statistics)
 
 # 4) Compute DataFrame with times spent for each questions to get each user answer
 
@@ -31,11 +31,11 @@ df_times_for_question.to_csv("datasets/results/times_for_questions.csv", index=F
 
 # 5) Compute DataFrame with some statistics related to times spent for each questions to get each user answer
 
-df_average_times_for_questions = statistics.time_statistics.get_average_times_for_questions_df(df_times_for_question, n_users)
-df_average_times_for_questions.to_csv("datasets/results/average_times_for_questions.csv", index=False)
-statistics.plots.get_answers_times_statistics_bar_plot(df_average_times_for_questions)
+df_statistics_times_for_questions = statistics.time_statistics.get_statistics_times_for_questions_df(df_times_for_question, n_users)
+df_statistics_times_for_questions.to_csv("datasets/results/statistics_times_for_questions.csv", index=False)
+statistics.plots.get_answers_times_statistics_bar_plot(df_statistics_times_for_questions)
 
 # 6) Compute DataFrame with all the information related to each user answer
 
-df_answer_complete_all_info = labelling.set_labels.get_answer_complete_all_info_df(n_users)
-df_answer_complete_all_info.to_csv('datasets/results/answers_complete_all_info.csv', index=False)
+# df_answer_complete_all_info = labelling.set_labels.get_answer_complete_all_info_df(n_users)
+# df_answer_complete_all_info.to_csv('datasets/results/answers_complete_all_info.csv', index=False)
--- statistics/time_statistics.py
+++ statistics/time_statistics.py
@@ -1,4 +1,4 @@
-from numpy import mean
+from numpy import mean, std
 import pandas as pd
 import toml
 
@@ -31,12 +31,13 @@ def get_answers_times_df(df_correct_answer, n_users):
     return df_times_for_question
 
 
-def get_average_times_for_questions_df(df_times_for_question, n_users):
+def get_statistics_times_for_questions_df(df_times_for_question, n_users):
 
-    df_average_times_for_questions = pd.DataFrame(columns=['MEDIA_NAME', 'AVERAGE_TIME'])
+    df_average_times_for_questions = pd.DataFrame(columns=['MEDIA_NAME', 'AVERAGE_TIME', 'STANDARD_DEVIATION'])
     df_average_times_for_questions['MEDIA_NAME'] = df_times_for_question['MEDIA_NAME']
 
     average_times = []
+    standard_deviations = []
 
     for i in df_times_for_question.index:
         list_times = []
@@ -44,8 +45,11 @@ def get_average_times_for_questions_df(df_times_for_question, n_users):
             if j not in config['general']['excluded_users']:
                 list_times.append(df_times_for_question['USER_' + str(j)][i])
         average_time = mean(list_times)
+        standard_deviation = std(list_times)
         average_times.append(average_time)
+        standard_deviations.append(standard_deviation)
 
     df_average_times_for_questions['AVERAGE_TIME'] = average_times
+    df_average_times_for_questions['STANDARD_DEVIATION'] = standard_deviations
 
     return df_average_times_for_questions
