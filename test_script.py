2b256cd8936e896871e49b43103a546a061b4aea
Gianantonio Confalonieri <gianantonioconfalonieri@gmail.com>
2022-05-17 21:13:45 +0200

    update test.py

--- test_script.py
+++ test_script.py
@@ -1,5 +1,10 @@
 import toml
+
 import pandas as pd
+from keras import Sequential
+from keras.layers import LSTM, Dense
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 config = toml.load('config.toml')
 
@@ -17,6 +22,24 @@ for i in range(1, 53):
             user_list = reduced_df['beta1'].tolist()
             complete_x_list.append(user_list)
 
+
 path_labelled_df = config['path']['labelled_dataset']
 df_labelled = pd.read_csv(path_labelled_df)
-complete_y_list = df_labelled['LABEL'].tolist()
+
+complete_x_list = np.array(complete_x_list)
+complete_y_list = np.array(df_labelled['LABEL'])
+X_train, X_test, y_train, y_test = train_test_split(complete_x_list, complete_y_list, test_size=0.2)
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+X_train = X_train.reshape(-1, 1, X_train.shape[1])
+
+model = Sequential()
+model.add(LSTM(100, return_sequences=True))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=3, batch_size=64)
+print(model.summary())
