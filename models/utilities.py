97cd16da0d16861081b68da0d5f8094a118db994
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-05-30 14:49:38 +0200

    add algorithm library

---
+++ models/utilities.py
@@ -0,0 +1,99 @@
+import toml
+import pandas as pd
+import numpy as np
+
+config = toml.load('config.toml')
+
+
+def get_max_series_len():
+
+    max_len = 0
+
+    for i in range(1, 53):
+        user_id = 'USER_' + str(i)
+        if i not in config['general']['excluded_users']:
+            path = 'datasets/sync_datasets/sync_dataset_' + user_id.lower() + '.csv'
+            df_sync = pd.read_csv(path)
+            media_names = df_sync.drop_duplicates('media_name', keep='last')['media_name']
+            for name in media_names:
+                reduced_df = df_sync[df_sync['media_name'] == name]
+                curr_len = len(reduced_df['time'])
+                if curr_len > max_len:
+                    max_len = curr_len
+
+    return max_len
+
+
+def get_users_array():
+
+    complete_x_list = []
+
+    for i in range(1, 53):
+        user_id = 'USER_' + str(i)
+        if i not in config['general']['excluded_users']:
+            user_list = []
+            path = 'datasets/sync_datasets/sync_dataset_' + user_id.lower() + '.csv'
+            df_sync = pd.read_csv(path)
+            media_names = df_sync.drop_duplicates('media_name', keep='last')['media_name']
+            for name in media_names:
+                reduced_df = df_sync[df_sync['media_name'] == name]
+                question_list = []
+                for f in config['algorithm']['eeg_features']:
+                    question_list.append(np.asarray(reduced_df[f]))
+                user_list.append(question_list)
+            complete_x_list.append(user_list)
+
+    return np.array(complete_x_list, dtype=np.ndarray)
+
+
+def get_questions_array():
+
+    complete_x_list = []
+
+    for i in range(1, 53):
+        user_id = 'USER_' + str(i)
+        if i not in config['general']['excluded_users']:
+            path = 'datasets/sync_datasets/sync_dataset_' + user_id.lower() + '.csv'
+            df_sync = pd.read_csv(path)
+            media_names = df_sync.drop_duplicates('media_name', keep='last')['media_name']
+            for name in media_names:
+                question_list = []
+                reduced_df = df_sync[df_sync['media_name'] == name]
+                for f in config['algorithm']['eeg_features']:
+                    question_list.append(np.asarray(reduced_df[f]))
+                complete_x_list.append(question_list)
+
+    return np.array(complete_x_list, dtype=np.ndarray)
+
+
+def get_labels_users_array():
+
+    c = 0
+    complete_y_list = []
+
+    path_labelled_df = config['path']['labelled_dataset']
+    df_labelled = pd.read_csv(path_labelled_df)
+
+    for i in range(1, 53):
+        if i not in config['general']['excluded_users']:
+            user_list = []
+            for j in range(1, 25):
+                user_list.append(df_labelled['LABEL'][c])
+                c += 1
+            complete_y_list.append(user_list)
+
+    return np.asarray(complete_y_list).astype('int')
+
+
+def get_labels_questions_array():
+
+    c = 0
+    complete_y_list = []
+
+    path_labelled_df = config['path']['labelled_dataset']
+    df_labelled = pd.read_csv(path_labelled_df)
+
+    for i in df_labelled.index:
+        complete_y_list.append(df_labelled['LABEL'][i])
+
+    return np.asarray(complete_y_list).astype('int')
--- test_script.py
+++ test_script.py
@@ -1,4 +1,7 @@
 import os
+
+import models.utilities
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.utils.np_utils import to_categorical
 import tensorflow as tf
@@ -10,47 +13,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import toml
 
+complete_x_list = models.utilities.get_questions_array()
+complete_y_list = models.utilities.get_labels_questions_array()
 
-config = toml.load('config.toml')
-
-features = ['beta1', 'beta2']
-
-complete_x_list = []
-complete_y_list = []
-
-users = []
-
-
-for i in range(1, 53):
-    user_id = 'USER_' + str(i)
-    if i not in config['general']['excluded_users']:
-        user_list = []
-        users.append(user_id)
-        path = 'datasets/sync_datasets/sync_dataset_' + user_id.lower() + '.csv'
-        df_sync = pd.read_csv(path)
-        media_names = df_sync.drop_duplicates('media_name', keep='last')['media_name']
-        for name in media_names:
-            reduced_df = df_sync[df_sync['media_name'] == name]
-            question_list = []
-            for f in features:
-                question_list.append(np.asarray(reduced_df[f]))
-            user_list.append(question_list)
-        complete_x_list.append(user_list)
-
-path_labelled_df = config['path']['labelled_dataset']
-df_labelled = pd.read_csv(path_labelled_df)
-
-c = 0
-for i in range(1, 53):
-    if i not in config['general']['excluded_users']:
-        user_list = []
-        for j in range(1, 25):
-            user_list.append(df_labelled['LABEL'][c])
-            c += 1
-        complete_y_list.append(user_list)
-
-complete_x_list = np.array(complete_x_list, dtype=np.ndarray)
-complete_y_list = np.asarray(complete_y_list).astype('float32')
+print(models.utilities.get_max_series_len())
 
 print("# TRAIN SERIES #")
 print(complete_x_list.shape)
@@ -61,24 +27,21 @@ print(complete_x_list[0][0].shape)
 print(type(complete_x_list[0][0]))
 print(complete_x_list[0][0][0].shape)
 print(type(complete_x_list[0][0][0]))
-print(type(complete_x_list[0][0][0][0]))
 
 print("# TRAIN LABELS #")
 print(complete_y_list.shape)
 print(type(complete_y_list))
 print(complete_y_list[0].shape)
 print(type(complete_y_list[0]))
-print(complete_y_list[0][0].shape)
-print(type(complete_y_list[0][0]))
 
-X_train, X_test, y_train, y_test = train_test_split(complete_x_list, complete_y_list, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(np.ones((46 * 24, 2, 100)), np.ones((46 * 24, 1)), test_size=0.2)
 
 # y_train = to_categorical(y_train)
 # y_test = to_categorical(y_test)
 
 model = Sequential()
 model.add(Conv1D(filters=256, kernel_size=2))
-model.add(MaxPooling1D(pool_size=2))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
