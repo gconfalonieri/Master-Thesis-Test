adcd8e936c93e4073d5f24b2efc10d13adf44416
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-06-05 12:21:39 +0200

    add functions to undersample arrays

--- models/utilities.py
+++ models/utilities.py
@@ -144,6 +144,35 @@ def get_users_oversampled_array():
     return np.array(complete_x_list, dtype=np.ndarray)
 
 
+def get_users_undersampled_array():
+
+    complete_x_list = []
+    min_len = get_min_series_len()
+
+    for i in range(1, 53):
+        user_id = 'USER_' + str(i)
+        if i not in config['general']['excluded_users']:
+            user_list = []
+            path = 'datasets/sync_datasets/sync_dataset_' + user_id.lower() + '.csv'
+            df_sync = pd.read_csv(path)
+            media_names = df_sync.drop_duplicates('media_name', keep='last')['media_name']
+            for name in media_names:
+                reduced_df = df_sync[df_sync['media_name'] == name]
+                question_list = []
+                for f in config['algorithm']['eeg_features']:
+                    arr = np.asarray(reduced_df[f]).astype('float32')
+                    oversampled_array = numpy.array(0)
+                    if config['preprocessing']['resample_library'] == 'sklearn':
+                        oversampled_array = sklearn.utils.resample(arr, n_samples=min_len, stratify=arr)
+                    elif config['preprocessing']['resample_library'] == 'scipy':
+                        oversampled_array = scipy.signal.resample(arr, min_len)
+                    question_list.append(oversampled_array)
+                user_list.append(question_list)
+            complete_x_list.append(user_list)
+
+    return np.array(complete_x_list, dtype=np.ndarray)
+
+
 def get_questions_array():
 
     complete_x_list = []
@@ -211,6 +240,34 @@ def get_questions_oversampled_array():
                         oversampled_array = sklearn.utils.resample(arr, n_samples=max_len, stratify=arr)
                     elif config['preprocessing']['resample_library'] == 'scipy':
                         oversampled_array = scipy.signal.resample(arr, max_len)
+                    question_list.append(oversampled_array)
+                complete_x_list.append(question_list)
+
+    return np.array(complete_x_list, dtype=np.ndarray)
+
+
+def get_questions_undersampled_array():
+
+    complete_x_list = []
+
+    min_len = get_min_series_len()
+
+    for i in range(1, 53):
+        user_id = 'USER_' + str(i)
+        if i not in config['general']['excluded_users']:
+            path = 'datasets/sync_datasets/sync_dataset_' + user_id.lower() + '.csv'
+            df_sync = pd.read_csv(path)
+            media_names = df_sync.drop_duplicates('media_name', keep='last')['media_name']
+            for name in media_names:
+                question_list = []
+                reduced_df = df_sync[df_sync['media_name'] == name]
+                for f in config['algorithm']['eeg_features']:
+                    arr = np.asarray(reduced_df[f]).astype('float32')
+                    oversampled_array = numpy.array(0)
+                    if config['preprocessing']['resample_library'] == 'sklearn':
+                        oversampled_array = sklearn.utils.resample(arr, n_samples=min_len, stratify=arr)
+                    elif config['preprocessing']['resample_library'] == 'scipy':
+                        oversampled_array = scipy.signal.resample(arr, min_len)
                     question_list.append(oversampled_array)
                 complete_x_list.append(question_list)
 
--- test_script.py
+++ test_script.py
@@ -18,17 +18,27 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import toml
 
-all_windowed_array_data = load('all_windowed_array_data.npy', allow_pickle=True)
-all_windowed_array_labels = load('all_windowed_array_labels.npy')
+loss_list = []
+accuracy_list = []
 
-complete_x_list = np.expand_dims(all_windowed_array_data[0], axis=1)
-complete_y_list = np.expand_dims(all_windowed_array_labels[1], axis=1)
+def get_model_1():
+    model = Sequential()
+    model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=4))
+    model.add(LSTM(32))
+    model.add(Dense(1, activation='linear'))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    return model
+
+complete_x_list = models.utilities.get_users_padded_array()
+complete_y_list = models.utilities.get_labels_users_array()
 
 print("# TRAIN SERIES #")
 print(complete_x_list.shape)
 
 print("# TRAIN LABELS #")
 print(complete_y_list.shape)
+
 
 # X_train, X_test, y_train, y_test = train_test_split(np.ones((46 * 24, 2, 100)), np.ones((46 * 24, 1)), test_size=0.2)
 X_train, X_test, y_train, y_test = train_test_split(complete_x_list, complete_y_list, test_size=0.2, shuffle=True)
@@ -39,21 +49,25 @@ X_test = np.asarray(X_test).astype(np.float32)
 # y_train = to_categorical(y_train)
 # y_test = to_categorical(y_test)
 
-model = Sequential()
-model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu'))
-model.add(MaxPooling1D(pool_size=4))
-model.add(LSTM(32))
-model.add(Dense(1, activation='linear'))
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model = get_model_1()
 
-history = model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+
 history_dict = history.history
 
-models.plots.plot_model_loss(history_dict)
-plt.clf()
-models.plots.plot_model_accuracy(history_dict)
+#models.plots.plot_model_loss(history_dict)
+#plt.clf()
+#models.plots.plot_model_accuracy(history_dict)
 
 results = model.evaluate(X_test, y_test)
-print(results)
+
+loss_list.append(results[0])
+accuracy_list.append(results[1])
+
+df_results = pd.DataFrame(columns=['loss', 'accuracy'])
+
+df_results['loss'] = results[0]
+df_results['accuracy'] = results[1]
+df_results.to_csv('model_results/questions_oversampled.csv', index=False)
 
 # print(model.summary())
