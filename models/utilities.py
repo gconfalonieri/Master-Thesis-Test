652abb045891372e6e6aecd85233f1e80ab55118
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-06-14 14:23:48 +0200

    refactor

--- models/utilities.py
+++ models/utilities.py
@@ -264,10 +264,10 @@ def get_questions_interpolation_array():
                 question_list = []
                 reduced_df = df_sync[df_sync['media_name'] == name]
                 time = np.asarray(reduced_df['time']).astype('float32')
-                for f in config['algorithm']['gaze_features']:
+                for f in config['algorithm']['eeg_features']:
                     arr = np.asarray(reduced_df[f]).astype('float32')
-                    interp_funct = interp1d(time, arr, kind = config['preprocessing']['interpolation_kind'])
                     new_time = np.linspace(time[0], time[len(time)-1], max_len)
+                    interp_funct = interp1d(time, arr, kind=config['preprocessing']['interpolation_kind'])
                     interpolated_array = interp_funct(new_time)
                     question_list.append(interpolated_array)
                 complete_x_list.append(question_list)
