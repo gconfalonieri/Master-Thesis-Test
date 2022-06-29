002da96a19ec5dff7daeef4918bd9783d8115393
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-06-29 11:38:07 +0200

    test with CNN1D

--- models/deep_learning_models.py
+++ models/deep_learning_models.py
@@ -2,7 +2,7 @@ import os
 from keras import Sequential, regularizers, Input
 from keras.regularizers import l1, l2, l1_l2
 from keras.layers import Conv1D, MaxPooling1D, Dense, LSTM, BatchNormalization, Conv2D, MaxPooling2D, Dropout, \
-    TimeDistributed, Flatten, InputLayer
+    TimeDistributed, Flatten, InputLayer, Reshape
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -19,9 +19,10 @@ def get_model_dense_cnn1d_lstm():
 def get_model_ccn1d(complete_x_list):
     model = Sequential()
     model.add(InputLayer(input_shape=complete_x_list[0].shape))
-    model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01), input_shape=(complete_x_list[0].shape)))
+    model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
     model.add(MaxPooling1D(pool_size=4, padding='same'))
-    model.add(Dense(1, activation='linear'))
+    model.add(Reshape(complete_x_list[0].shape))
+    model.add(Dense(1, activation='linear', input_shape=()))
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
     return model
 
