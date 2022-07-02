971d171cdf1b521718599bb7968e95d2f6cd39f5
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-07-02 11:54:08 +0200

    cuda test

--- models/deep_learning_models.py
+++ models/deep_learning_models.py
@@ -4,8 +4,6 @@ from keras.regularizers import l1, l2, l1_l2
 from keras.layers import Conv1D, MaxPooling1D, Dense, LSTM, BatchNormalization, Conv2D, MaxPooling2D, Dropout, \
     TimeDistributed, Flatten, InputLayer, Reshape
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 # activation = relu
 # filters = 32 / altri valori
 # In tutti i casi dense a inizio e fine
@@ -20,13 +18,11 @@ def get_model_dense_cnn1d_lstm():
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
     return model
 
-def get_model_ccn1d(complete_x_list):
+def get_model_ccn1d():
     model = Sequential()
-    model.add(InputLayer(input_shape=complete_x_list[0].shape))
     # Dense da mettere
     model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
     model.add(MaxPooling1D(pool_size=4, padding='same'))
-    # model.add(Flatten())
     model.add(Dense(1, activation='linear'))
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
     return model
@@ -65,9 +61,8 @@ def get_model_2x_cnn1d_lstm(complete_x_list):
     return model
 
 
-def get_model_cnn1d_lstm_3x_dense(complete_x_list):
+def get_model_cnn1d_lstm_3x_dense():
     model = Sequential()
-    model.add(InputLayer(input_shape=complete_x_list[0].shape))
     model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu'))
     model.add(MaxPooling1D(pool_size=4, padding='same'))
     model.add(LSTM(32))
@@ -78,9 +73,8 @@ def get_model_cnn1d_lstm_3x_dense(complete_x_list):
     return model
 
 
-def get_model_cnn2d(complete_x_list):
+def get_model_cnn2d():
     model = Sequential()
-    # model.add(InputLayer(input_shape=complete_x_list[0].shape))
     model.add(Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=3, padding='same'))
     model.add(Dense(1, activation='linear'))
@@ -88,9 +82,8 @@ def get_model_cnn2d(complete_x_list):
     return model
 
 
-def get_model_cnn2d_lstm(complete_x_list):
+def get_model_cnn2d_lstm():
     model = Sequential()
-    # model.add(InputLayer(input_shape=complete_x_list[0].shape))
     model.add(Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=3, padding='same'))
     model.add(Dropout(0.2))
