331329d4ecbdd4b333ccdd618c83f99c17037610
gianantonioconfalonieri <gianantonioconfalonieri@gmail.com>
2022-07-05 14:42:05 +0200

    update test.py

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ from sklearn.model_selection import train_test_split
 import models
 
 complete_x_list = models.utilities.get_questions_padded_array()
-np.save('datasets/arrays/undersampled/input_0_1_padded_begin.npy', complete_x_list)
+np.save('datasets/arrays/undersampled/input_1_1_padded_begin.npy', complete_x_list)
