5a148316b3d53518d1c702a1b4db10f4acff3d6a
Gianantonio Confalonieri <gianantonioconfalonieri@gmail.com>
2022-07-25 15:01:54 +0200

    add experiments with thr

--- prova.py
+++ prova.py
@@ -10,6 +10,7 @@ from scipy.interpolate import interp1d
 from sklearn.model_selection import train_test_split
 
 import models
+from models.utilities import get_min_series_len_shifted
 
 config = toml.load('config.toml')
 
@@ -36,55 +37,57 @@ def get_questions_oversampled_validation_shifted(test_size_value):
                 for j in range(0, 14):
                     shifted = reduced_df.iloc[j::15, :]
                     feature_list = []
-                    for f in config['algorithm']['gaze_features']:
-                        arr = np.asarray(shifted[f]).astype('float32')
-                        oversampled_array = numpy.array(0)
-                        if config['preprocessing']['resample_library'] == 'sklearn':
-                            oversampled_array = sklearn.utils.resample(arr, n_samples=max_len, stratify=arr)
-                        elif config['preprocessing']['resample_library'] == 'scipy':
-                            oversampled_array = scipy.signal.resample(arr, max_len)
-                        feature_list.append(oversampled_array)
-                    question_list.append(feature_list)
-                red_df = df_labelled.loc[(df_labelled['USER_ID'] == user_id) & (df_labelled['MEDIA_NAME'] == name)]
-                arr = np.array(red_df['LABEL'].values[0])
-                complete_y.append(np.expand_dims(arr, axis=(0)))
-                complete_x.append(question_list)
+                    counter = 0
+                    if len(shifted) > 20:
+                        for f in config['algorithm']['gaze_features']:
+                            arr = np.asarray(shifted[f]).astype('float32')
+                            oversampled_array = numpy.array(0)
+                            if config['preprocessing']['resample_library'] == 'sklearn':
+                                oversampled_array = sklearn.utils.resample(arr, n_samples=max_len, stratify=arr)
+                            elif config['preprocessing']['resample_library'] == 'scipy':
+                                oversampled_array = scipy.signal.resample(arr, max_len)
+                            feature_list.append(oversampled_array)
+                        question_list.append(feature_list)
+                if len(question_list) > 0:
+                    complete_x.append(question_list)
+                    red_df = df_labelled.loc[(df_labelled['USER_ID'] == user_id) & (df_labelled['MEDIA_NAME'] == name)]
+                    arr = np.array(red_df['LABEL'].values[0])
+                    complete_y.append(np.expand_dims(arr, axis=(0)))
 
     array_x = np.array(complete_x, dtype=np.ndarray)
     array_y = np.asarray(complete_y).astype('int')
 
-    train_array, validation_array, train_labels, validation_labels = train_test_split(array_x, array_y, test_size=test_size_value, shuffle=True)
+    train_array, validation_array, label_train, label_validation = train_test_split(array_x, array_y, test_size=test_size_value, shuffle=True)
 
     new_train = []
     new_validation = []
     new_label_train = []
     new_label_validation = []
 
+    index = 0
     for x in train_array:
         for y in x:
             new_train.append(y)
+            new_label_train.append(label_train[index])
+        index += 1
+    index = 0
     for x in validation_array:
         for y in x:
             new_validation.append(y)
-    for x in train_labels:
-        for i in range(0, 14):
-            new_label_train.append(x)
-    for x in validation_labels:
-        for i in range(0, 14):
-            new_label_validation.append(x)
+            new_label_validation.append(label_validation[index])
+        index += 1
 
     return np.array(new_train).astype(np.float32), np.asarray(new_validation).astype(np.float32), \
            np.asarray(new_label_train).astype('int'), np.asarray(new_label_validation).astype('int')
 
+
+# total_arr2 = models.utilities.get_arrays_shuffled_shifted(0.12)
 
 total_arr = get_questions_oversampled_validation_shifted(0.12)
 X_train = total_arr[0]
 X_test = total_arr[1]
 y_train = total_arr[2]
 y_test = total_arr[3]
-
-print(X_train.shape)
-print(type(X_train))
 
 model = models.deep_learning_models.get_model_lstm(0, 0, '', 'relu', 32, 'mse', 'adam', 1, 0.1)
 history = model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test), shuffle=True)
